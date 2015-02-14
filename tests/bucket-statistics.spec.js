1430c8c46658d00f081e787063f56d411b52eeae
Markus <mail.markus.ziller@gmail.com>
2015-02-14 21:03:53 +0100

    Created specs for bucket-statistics in bucket-statistics.spec.js

--- tests/bucket-statistics.spec.js
+++ tests/bucket-statistics.spec.js
@@ -1,12 +1,50 @@
-var RebuildIndexesLogic = require('../lib/bucket-index/bucket-index');
+var RiakMockServer = require('riak-mock-server');
+var RiakJS = require('riak-js');
+var RiakPopulator = require('../lib/bucket-populator/riak_populator');
 
 var expect = require('expect.js');
-var chai = require('chai');
-var assert = chai.assert;
-var config = {};
-var db = require('riak-js').getClient(config);
+//var chai = require('chai');
+//var assert = chai.assert;
 
+var BucketStatistics = require('../lib/bucket-statistics/bucket-statistics');
+describe.only('BucketStatistics', function() {
+  var bucketName = 'test_bucket';
+  var port = 9999;
+  var riakConfig = { host: 'localhost', port: port };
+  var riakClient = RiakJS(riakConfig);
 
-describe('Bucket index', function() {
+  var populatorConfig = {};
+  populatorConfig[bucketName] = {
+    createNewInstance: function(id) {
+      return {
+        getBody: function() {
+          return { my_numeric_value: id, my_string_value: 'number_' + id };
+        }
+      };
+    },
+    count: 100
+  };
 
+  var riakServer = new RiakMockServer({ port: port });
+  var riakPopulator = new RiakPopulator(riakConfig, populatorConfig);
+
+  before(function(done) {
+    riakServer.start(function(port) {
+      riakPopulator.start(function() {
+        expect(port).to.be.ok();
+        done();
+      });
+    });
+  });
+
+  after(function(done) {
+    riakServer.stop(done);
+  });
+
+  it('receive no results for a non existing bucket', function(done) {
+    BucketStatistics.calculateStatistics(bucketName, riakClient, null, function(result) {
+      expect(result.count.elements).to.be(100);
+      done();
+    });
+  });
 });
