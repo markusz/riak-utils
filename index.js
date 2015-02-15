e642eda99c68e85d1d71f086de130b5bb9d4947c
Markus Z <markus.ziller@netlight.com>
2015-02-15 14:46:16 +0100

    Exported libs in index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 module.exports = {
-	AbstractRecord: require('./lib/records/abstract_record'),
-	Bucket: require('./lib/datastores/bucket'),
-	getBucketHandler: function(bucketName){
-		var Bucket = require('./lib/datastores/bucket')
-		return new Bucket(bucketName);
-	}
+  BucketCleanup: require('./lib/bucket-cleanup/bucket-cleanup'),
+  BucketCompare: require('./lib/bucket-compare/bucket-compare'),
+  BucketExport: require('./lib/bucket-export/bucket-export'),
+  BucketIndex: require('./lib/bucket-index/bucket-index'),
+  BucketPopulate: require('./lib/bucket-populate/bucket-populate'),
+  BucketStatistics: require('./lib/bucket-statistics/bucket-statistics'),
+  BucketTransfer: require('./lib/bucket-transfer/bucket-transfer')
 };
