8ccd3091c073dbca43a575ea42129a344e06fdcc
Markus Z <markus.ziller@netlight.com>
2015-02-16 00:04:13 +0100

    Changed exports in index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
   BucketCleanup: require('./lib/bucket-cleanup/bucket-cleanup'),
-  BucketCompare: require('./lib/bucket-compare/bucket-compare'),
+  //BucketCompare: require('./lib/bucket-compare/bucket-compare'),
   BucketExport: require('./lib/bucket-export/bucket-export'),
-  BucketIndex: require('./lib/bucket-index/bucket-index'),
+  //BucketIndex: require('./lib/bucket-index/bucket-index'),
   BucketPopulate: require('./lib/bucket-populate/bucket-populate'),
   BucketStatistics: require('./lib/bucket-statistics/bucket-statistics'),
   BucketTransfer: require('./lib/bucket-transfer/bucket-transfer')
